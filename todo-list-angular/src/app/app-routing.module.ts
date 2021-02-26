ed69944d954664b1c49d6b4bcc1dec9cf2179c51
itsokto <36706257+itsokto@users.noreply.github.com>
2021-02-26 11:46:11 +0300

    todo-list-angular: add routing

--- todo-list-angular/src/app/app-routing.module.ts
+++ todo-list-angular/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
+import { RegisterComponent } from './register/register.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: '',
+    component: TodoListComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
