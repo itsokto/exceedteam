4b3314a8727312732883432469b2189b74e34bd8
itsokto <36706257+itsokto@users.noreply.github.com>
2021-02-26 11:52:01 +0300

    todos-api: token verification

---
+++ todos-api/app/middlewares/verify-token.js
@@ -0,0 +1,17 @@
+const jwtTokenService = require("../services/jwt.token.service");
+
+function verifyToken(req, res, next) {
+  const authHeader = req.headers.authorization;
+  const token = authHeader?.split(" ")[1];
+
+  if (!token) return res.status(403).send({ message: "No token provided." });
+
+  jwtTokenService.verify(token, function (err, decoded) {
+    if (err) return res.status(401).send({ message: "Token is expired." });
+
+    req.user = decoded;
+    next();
+  });
+}
+
+module.exports = verifyToken;
