a7017a35bead97e14a114de1dee1f62c3be8221b
itsokto <36706257+itsokto@users.noreply.github.com>
2021-02-26 11:43:22 +0300

    todo-list-angular: create auth.service

---
+++ todo-list-angular/src/app/services/auth.service.ts
@@ -0,0 +1,39 @@
+import { AuthResponse } from './../types/auth.response';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private baseUrl = 'http://localhost:8080/api/auth';
+
+  constructor(private httpClient: HttpClient) {}
+
+  login(name: string, password: string): Observable<AuthResponse> {
+    const url = `${this.baseUrl}/login`;
+    return this.httpClient.post<AuthResponse>(url, {
+      name: name,
+      password: password,
+    });
+  }
+
+  register(name: string, password: string): Observable<AuthResponse> {
+    const url = `${this.baseUrl}/register`;
+
+    return this.httpClient.post<AuthResponse>(url, {
+      name: name,
+      password: password,
+    });
+  }
+
+  refresh(name: string, password: string): Observable<AuthResponse> {
+    const url = `${this.baseUrl}/refresh`;
+
+    return this.httpClient.post<AuthResponse>(url, {
+      name: name,
+      password: password,
+    });
+  }
+}
