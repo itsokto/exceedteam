2fa3af45c5c0ede051c1f7a6549fc58aaed40d31
itsokto <36706257+itsokto@users.noreply.github.com>
2021-03-05 12:39:42 +0300

    todo-list-angular: move paper shadow to tailwind theme

--- todo-list-angular/tailwind.config.js
+++ todo-list-angular/tailwind.config.js
@@ -31,6 +31,9 @@ module.exports = {
         secondary: '#79b5ab',
         background: '#f5f5f5'
       },
+      boxShadow: {
+        paper: '0 1px 1px rgb(209, 213, 219), 0 10px 0 -5px #fff, 0 10px 1px -4px rgb(209, 213, 219), 0 20px 0 -10px #fff, 0 20px 1px -9px rgb(209, 213, 219);'
+      }
     }
   }
 }
